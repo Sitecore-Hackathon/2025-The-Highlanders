c13a8ab1b21f40558cdf8b5a361af7ea9dd3b298
Ramiro Batallas <rbatallas@oshyn.com>
2025-03-08 05:32:06 -0500

    add call to AI Service

---
+++ src/Foundation/Services/code/Models/AIModels.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Highlanders.Foundation.Services.Models
+{
+    public partial class AIQuestion
+    {
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("messages")]
+        public List<Message> Messages { get; set; }
+
+        [JsonProperty("temperature")]
+        public long Temperature { get; set; }
+
+        [JsonProperty("max_tokens")]
+        public long MaxTokens { get; set; }
+
+        [JsonProperty("top_p")]
+        public long TopP { get; set; }
+
+        [JsonProperty("frequency_penalty")]
+        public long FrequencyPenalty { get; set; }
+
+        [JsonProperty("presence_penalty")]
+        public long PresencePenalty { get; set; }
+
+        [JsonProperty("response_format")]
+        public ResponseFormat ResponseFormat { get; set; }
+
+        public AIQuestion()
+        {
+            ResponseFormat = new ResponseFormat();
+        }
+    }
+
+    public partial class Message
+    {
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("content")]
+        public List<Content> Content { get; set; }
+
+        public Message()
+        {
+            Content = new List<Content>();
+        }
+    }
+
+    public partial class Content
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public partial class ResponseFormat
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+
+
+    public partial class AiAnswer
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("created")]
+        public long Created { get; set; }
+
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("choices")]
+        public List<Choice> Choices { get; set; }
+
+        [JsonProperty("usage")]
+        public Usage Usage { get; set; }
+
+        [JsonProperty("service_tier")]
+        public string ServiceTier { get; set; }
+
+        [JsonProperty("system_fingerprint")]
+        public string SystemFingerprint { get; set; }
+
+        public AiAnswer()
+        {
+            Usage = new Usage();
+            Choices = new List<Choice>();
+        }
+    }
+
+    public partial class Choice
+    {
+        [JsonProperty("index")]
+        public long Index { get; set; }
+
+        [JsonProperty("message")]
+        public MessageAnswer MessageAnswer { get; set; }
+
+        [JsonProperty("logprobs")]
+        public object Logprobs { get; set; }
+
+        [JsonProperty("finish_reason")]
+        public string FinishReason { get; set; }
+
+        public Choice()
+        {
+            MessageAnswer = new MessageAnswer();
+        }
+    }
+
+    public partial class MessageAnswer
+    {
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("refusal")]
+        public object Refusal { get; set; }
+    }
+
+    public partial class Usage
+    {
+        [JsonProperty("prompt_tokens")]
+        public long PromptTokens { get; set; }
+
+        [JsonProperty("completion_tokens")]
+        public long CompletionTokens { get; set; }
+
+        [JsonProperty("total_tokens")]
+        public long TotalTokens { get; set; }
+
+        [JsonProperty("prompt_tokens_details")]
+        public PromptTokensDetails PromptTokensDetails { get; set; }
+
+        [JsonProperty("completion_tokens_details")]
+        public CompletionTokensDetails CompletionTokensDetails { get; set; }
+
+        public Usage()
+        {
+            PromptTokensDetails = new PromptTokensDetails();
+            CompletionTokensDetails = new CompletionTokensDetails();
+        }
+    }
+
+    public partial class CompletionTokensDetails
+    {
+        [JsonProperty("reasoning_tokens")]
+        public long ReasoningTokens { get; set; }
+
+        [JsonProperty("audio_tokens")]
+        public long AudioTokens { get; set; }
+
+        [JsonProperty("accepted_prediction_tokens")]
+        public long AcceptedPredictionTokens { get; set; }
+
+        [JsonProperty("rejected_prediction_tokens")]
+        public long RejectedPredictionTokens { get; set; }
+    }
+
+    public partial class PromptTokensDetails
+    {
+        [JsonProperty("cached_tokens")]
+        public long CachedTokens { get; set; }
+
+        [JsonProperty("audio_tokens")]
+        public long AudioTokens { get; set; }
+    }
+}
---
+++ src/Foundation/Services/code/ServiceLayer/AIIntegration.cs
@@ -0,0 +1,85 @@
+﻿using Highlanders.Foundation.Services.Models;
+using Newtonsoft.Json;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highlanders.Foundation.Services.ServiceLayer
+{
+    public class AIIntegration : IAIIntegration
+    {
+        public async Task<AiAnswer> GetAIResponse(string input)
+        {
+            var model = new AiAnswer();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var idTokenCookie = Settings.GetSetting("ChatGPTApiKey");
+                    var url = Settings.GetSetting("ChatGPTApiUrl");
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", idTokenCookie);
+
+                    var question = GetQuestionModel(input);
+                    var jsonQuestion = JsonConvert.SerializeObject(question, Formatting.Indented);
+                    StringContent content = new StringContent(jsonQuestion, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = await response.Content.ReadAsStringAsync();
+                        model = JsonConvert.DeserializeObject<AiAnswer>(data);
+                    }
+                    else
+                    {
+                        Log.Error("An error occured in fetching getBulletinsSearchData: " + response.StatusCode, this);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in AIIntegration.GetResponse", ex, this);
+            }
+
+            return model;
+        }
+
+        private AIQuestion GetQuestionModel(string input)
+        {
+            var question = new AIQuestion
+            {
+                Model = "gpt-4o",
+                Messages = new List<Message>
+                {
+                    new Message
+                    {
+                        Role = "user",
+                        Content = new List<Content>
+                        {
+                            new Content
+                            {
+                                Type = "text",
+                                Text = input
+                            }
+                        }
+                    }
+                },
+                Temperature = 1,
+                MaxTokens = 10000,
+                TopP = 1,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0,
+                ResponseFormat = new ResponseFormat
+                {
+                    Type = "text"
+                }
+            };
+            return question;
+        }
+    }
+}
