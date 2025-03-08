966f30ae7c544260c07f195f11de6c73e8d5695a
Freddy Rueda <frueda@oshyn.com>
2025-03-08 09:42:30 -0500

    Add serialization helper

--- src/Feature/GenerateTemplates/code/Helpers/SerializationHelper.cs
+++ src/Feature/GenerateTemplates/code/Helpers/SerializationHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,26 @@ namespace Highlanders.Feature.GenerateTemplates.Helpers
 {
     public class SerializationHelper
     {
+        public static void PushSerialization()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                WorkingDirectory = "C:\\Projects\\2025-The-Highlanders",
+                FileName = "dotnet",
+                Arguments = "sitecore ser push",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process { StartInfo = psi })
+            {
+                process.OutputDataReceived += (sender, args) => Debug.WriteLine(args.Data);
+                process.ErrorDataReceived += (sender, args) => Debug.WriteLine("ERROR: " + args.Data);
+
+                process.Start();
+            }
+        }
     }
 }
