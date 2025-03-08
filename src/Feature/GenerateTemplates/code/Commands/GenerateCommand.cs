8b1370a62f1648bc26fd6f4cc3632fa098e60dd5
Ramiro Batallas <rbatallas@oshyn.com>
2025-03-08 14:32:27 -0500

    ribbon command

---
+++ src/Feature/GenerateTemplates/code/Commands/GenerateCommand.cs
@@ -0,0 +1,41 @@
+﻿using Highlanders.Feature.GenerateTemplates.Repository;
+using Sitecore.DependencyInjection;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using System.Collections.Specialized;
+
+namespace Highlanders.Feature.GenerateTemplates.Commands
+{
+	public class GenerateCommand: Command
+    {
+        private readonly ICreateTemplatesRepository _CreateTemplatesRepository;
+        public GenerateCommand()
+        {
+            _CreateTemplatesRepository = (ICreateTemplatesRepository)ServiceLocator.ServiceProvider.GetService(typeof(ICreateTemplatesRepository));
+        }
+
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Context.ClientPage.Start(this, "Run", new NameValueCollection
+            {
+            });
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            if(!args.IsPostBack)
+            {
+                SheerResponse.Input("Describe the template you want to create. e.g: The data I want to register in this template is related to Blogs.", "Pront", string.Empty);
+                args.WaitForPostBack();
+            }
+            else
+            {
+                if (args.HasResult)
+                {
+                    string prontInput = args.Result;
+                    _CreateTemplatesRepository.CreateYmlFiles(prontInput);
+                }
+            }
+        }
+    }
+}
