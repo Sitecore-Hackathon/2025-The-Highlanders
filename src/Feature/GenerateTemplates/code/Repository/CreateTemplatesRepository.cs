294a4f1f90d187f9986754b389ca17a1f46fb9ef
Freddy Rueda <frueda@oshyn.com>
2025-03-08 14:07:09 -0500

    Integrate AI with creation of templates first version

--- src/Feature/GenerateTemplates/code/Repository/CreateTemplatesRepository.cs
+++ src/Feature/GenerateTemplates/code/Repository/CreateTemplatesRepository.cs
@@ -1,10 +1,14 @@
 ﻿using Highlanders.Feature.GenerateTemplates.Models;
 using Highlanders.Foundation.DependencyInjection;
+using Highlanders.Foundation.Services;
+using Highlanders.Foundation.Services.Models;
+using Highlanders.Foundation.Services.ServiceLayer;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Highlanders.Feature.GenerateTemplates.Repository
@@ -12,21 +16,32 @@ namespace Highlanders.Feature.GenerateTemplates.Repository
     [Service(typeof(ICreateTemplatesRepository))]
     public class CreateTemplatesRepository: ICreateTemplatesRepository
     {
-        public void CreateYmlFiles(string jsonYmlStructure)
+        IAIIntegration _iAIIntegration;
+
+        public CreateTemplatesRepository(IAIIntegration iAIIntegration)
         {
-            string json = "{\r\n  \"ymlFiles\": [\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Templates/Book/_Common.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/_Common\\\"\\nItemName: \\\"_Common\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Templates/Book/Book.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{3E2A6DE6-35D1-4565-B6B7-AC0D4F2839DB}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book\\\"\\nItemName: \\\"Book\\\"\\nSharedFields: []\\nUnversionedFields: []\\nVersionedFields: []\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Sections/Book/Book Information.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Book Information\\\"\\nItemName: \\\"Book Information\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Sections/Book/Publishing Details.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Publishing Details\\\"\\nItemName: \\\"Publishing Details\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Book Information/Title.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Book Information/Title\\\"\\nItemName: \\\"Title\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Single-Line Text\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Book Information/Author.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Book Information/Author\\\"\\nItemName: \\\"Author\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Single-Line Text\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Book Information/ISBN.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Book Information/ISBN\\\"\\nItemName: \\\"ISBN\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Single-Line Text\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Book Information/Summary.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Book Information/Summary\\\"\\nItemName: \\\"Summary\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Multi-Line Text\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Publishing Details/Publisher.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Publishing Details/Publisher\\\"\\nItemName: \\\"Publisher\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Single-Line Text\\\"\"\r\n    },\r\n    {\r\n      \"path\": \"/serialization/Feature/Library/Template Fields/Book/Publishing Details/PublishedDate.yml\",\r\n      \"content\": \"---\\nID: \\\"{GUID}\\\"\\nParent: \\\"{GUID}\\\"\\nTemplate: \\\"{GUID}\\\"\\nPath: \\\"/sitecore/templates/Feature/Library/Book/__Sections/Publishing Details/PublishedDate\\\"\\nItemName: \\\"PublishedDate\\\"\\nSharedFields:\\n- ID: \\\"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\\\"\\n  Value: \\\"Date\\\"\"\r\n    }\r\n  ]\r\n}";
+            _iAIIntegration = iAIIntegration;
+        }
 
-            var ymlFilesContainer = JsonConvert.DeserializeObject<YmlFileContainer>(json);
+        public void CreateYmlFiles(string requirement)
+        {
+            string prompt = "create and provide the Sitecore templates ymls based in Helix convention for items, remember that the template name is the main folder and its yml at the same level, after that, comes folders with the section name, with their corresponding ymls, and inside the section folders there are the fields with their corresponding ymls. The structure of the json result is {  'ymlFiles': [    {      'path': '',      'content': ''    }]} no explanations only the json as asked. The content part of the ymls of folders needs these fields with the pattern {GUID},  ID : {GUID}, Parent : {GUID}, Template : {GUID}, Path: [path/name.yml] (Different to the first path). The fields ymls needs SharedFields:  -ID: {GUID}, Hint: \"Type\", Value: Type of field (you have to give me a valid Sitecore type field), it is important the hyphen next to ID field because the next fields after SharedFields are indented inside it.. You have to mark which field is child of a folder, example Field1.Parent: {GUID1}, Field2.Parent:{GUID2}, I need this to create the GUIDs dynamically and map to the respective fields. "+ requirement+". This information will be used to create yml files with the following structure for fields ID: \"5ed51867-63a9-4152-b325-5c229fd03c00\" \r\nParent: \"e5f585ba-bea8-4b82-b8ec-a55a9a34fe9e\"\r\nTemplate: \"ab86861a-6030-46c5-b394-e8f99e8b87db\"\r\nPath: /sitecore/templates/Feature/Highlanders/Test\r\nSharedFields:\r\n- ID: \"12c33f3f-86c5-43a5-aeb4-5598cec45116\"\r\n  Hint: __Base template\r\n  Value: \"{1930BBEB-7805-471A-A3BE-4858AC7CF696}\" and for folder's yml ID: \"6a6226ff-0e94-4aa6-a1a9-a434d0941099\"\r\nParent: \"69d4a0c3-1482-4eaf-8237-a63344fc03a1\"\r\nTemplate: \"e269fbb5-3750-427a-9149-7aa950b49301\"\r\nPath: /sitecore/templates/Feature/Highlanders/Sample/General Be sure maintain both the folder and fields structure as asked. Every content should have also a path, see the examples well.";
+            Task<AiAnswer> json = _iAIIntegration.GetAIResponse(prompt);
 
+            AiAnswer answer = json.Result;
+
+            var ymlFilesContainer = JsonConvert.DeserializeObject<YmlFileContainer>(answer.Choices[0].MessageAnswer.Content.Replace("```json","").Replace("```","").Trim());
+
+            string mainPath = "C:\\Projects\\2025-The-Highlanders\\src\\Feature\\GenerateTemplates\\serialization\\Templates\\Highlanders\\";
             foreach (var file in ymlFilesContainer.YmlFiles)
             {
                 string directoryPath = Path.GetDirectoryName(file.Path);
-                if (!Directory.Exists(directoryPath))
+                if (!Directory.Exists(mainPath+directoryPath))
                 {
-                    Directory.CreateDirectory(directoryPath);
+                    Directory.CreateDirectory(mainPath+ directoryPath);
                 }
 
-                File.WriteAllText(file.Path, file.Content);
+                File.WriteAllText(mainPath + file.Path, file.Content);
             }
 
 
--- src/Foundation/Services/code/ServiceLayer/AIIntegration.cs
+++ src/Foundation/Services/code/ServiceLayer/AIIntegration.cs
@@ -8,9 +8,11 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Highlanders.Foundation.DependencyInjection;
 
 namespace Highlanders.Foundation.Services.ServiceLayer
 {
+    [Service(typeof(IAIIntegration))]
     public class AIIntegration : IAIIntegration
     {
         public async Task<AiAnswer> GetAIResponse(string input)
@@ -21,14 +23,16 @@ namespace Highlanders.Foundation.Services.ServiceLayer
                 using (var client = new HttpClient())
                 {
                     var idTokenCookie = Settings.GetSetting("ChatGPTApiKey");
-                    var url = Settings.GetSetting("ChatGPTApiUrl");
+                    var url = Settings.GetSetting("ChatGPTUrl");
 
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", idTokenCookie);
 
                     var question = GetQuestionModel(input);
                     var jsonQuestion = JsonConvert.SerializeObject(question, Formatting.Indented);
+                    //var jsonBody = $@"{{""model"": ""gpt-4o"",""messages"": [{{ ""role"": ""system"", ""content"": ""you are a content editor in sitecore"" }},{{ ""role"": ""user"", ""content"": ""Transform the given variable by replacing each word separated by pipes (|) with its corresponding Sitecore field type. Allowed Sitecore field types: Date,Datetime,Number,Single-Line Text,Rich Text.Classification Rules: If the word contains only numbers, classify it as Number. If the word matches a date format like dd/MM/yyyy or yyyy-MM-dd, classify it as Date. If the word matches a datetime format (including time), classify it as Datetime. If the word has more than 50 characters or has html tags, classify it as Rich Text. Respond only the replaced variable. Use this variable: abc|123"" }}]}}";
+
                     StringContent content = new StringContent(jsonQuestion, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    HttpResponseMessage response = client.PostAsync(url, content).Result;
 
                     if (response.IsSuccessStatusCode)
                     {
@@ -37,7 +41,7 @@ namespace Highlanders.Foundation.Services.ServiceLayer
                     }
                     else
                     {
-                        Log.Error("An error occured in fetching getBulletinsSearchData: " + response.StatusCode, this);
+                        Log.Error("An error occured in fetching OpenAI: " + response.StatusCode, this);
                     }
                 }
             }
